98311843f05f1c2b225a6dfd0cee90a6fcfd1f11
Matthew Kastor <matthewkastor@gmail.com>
2013-09-29 01:13:00 -0400

    regenerated from source.

--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -6,6 +6,15 @@
 
     window.atropa = {
         /// <summary></summary>
+        /// <field name="regex" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-regex/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="string" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-string/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="setAsOptionalArg" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-setAsOptionalArg/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
         /// <field name="wtf" type="">Container for all Glorious WTFifier related functions and such.</field>
         /// <returns type="atropa"/>
                 
